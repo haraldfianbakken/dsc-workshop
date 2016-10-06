663dd99d7caea18fe1adcd3bc226abb8184f5c03
Harald Fianbakken <harald.fianbakken@if.no>
2016-10-06 10:31:55 +0200

    Updated files

---
+++ Walkthrough.ps1
@@ -0,0 +1,65 @@
+# Create a first configuration 
+<# - File Resource, Windows Feature, Log , Script resource 
+   - Add parameters
+   - Look at MOFS
+   - Create config file
+   - Show LCM
+   - Show credentials/handling
+   - Generate certificates  
+#>
+
+# Remoting
+<#
+ - Create session
+ - Copy items
+ - Invoke-commands
+ - Pushing DSC configurations to remote
+#>
+
+# Package management
+<#
+ - Show commands 
+ - Show help, examples
+ - Create configuration using packagemanagement
+ - Create source for chocolatey
+ - Fetch package /install chocolatey
+ - Register nuget  / repo
+ - Install Myget package to destination
+#>
+
+# Composite resources
+<#
+ - Show folders structure
+ - Create module
+ - Create sub-resource
+ - Create splatted resource
+ - Create configuration and compile
+ - Run configuration 
+#>
+
+# Pull server
+<#
+    - Create pull server
+    - Configure pull client
+    - Create a partial document
+    - Compile, verify/checksum
+    - Update configuration & Start configuration 
+#>
+
+# Azure automation
+<#
+    - Create resource group for demo
+    - Create AAM account
+    - Create simple configuration, publish, push and compile
+    - Provision machine
+    - Show onboarding steps
+    - Show ARM template
+    - Show templates preview
+
+    
+#>
+
+# DevOps 
+<# 
+    Show Repository, show code, show pull requests 
+#>
